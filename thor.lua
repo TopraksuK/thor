b989060e596d1d32aad97d609955ed5cb31a16dc
topraksuk <toprak_karakaya@hotmail.com>
2024-04-21 21:26:39 +0300

    first commit

---
+++ thor.lua
@@ -0,0 +1,17 @@
+-- [Dependincies] --
+
+local shadowcraft
+
+if fs.find("shadowcraft") then
+    shadowcraft = require("shadowcraft")
+else
+    error("Shadowcraft v1.0.1+ is required to run THOR.")
+end
+
+-- [Objects] --
+
+local service = {}
+
+service = {
+    
+}
